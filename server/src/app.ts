6ad1b1a6e26895e3e5e80b4682c72a54eee286eb
Luiz Sócrate <luiz@socrate.com.br>
2020-01-20 23:09:28 +0100

    Enable cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import express from 'express'
 import helmet from 'helmet'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -12,6 +13,7 @@ export default async () => {
   app.use(helmet())
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(cors())
 
   app.get('/', (req, res) => {
     res.send('Uow')
