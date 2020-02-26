f9e9ff803ec826f46f817b95595ff18e5944384b
Martin Schröder <>
2020-02-26 22:49:21 +0100

    use table to display users

--- client/src/components/UserRow.tsx
+++ client/src/components/UserRow.tsx
@@ -1,35 +1,30 @@
 import React from 'react'
-import { Card, Container, Typography, Avatar, Grid } from '@material-ui/core'
+import { Avatar, TableRow, TableCell } from '@material-ui/core'
 import { UserRecord } from '../views/UserView'
 
 export function UserRow(props: { user: UserRecord }): JSX.Element {
   return (
-    <Container>
-      <Card>
-        <Grid container alignItems="center" justify="space-evenly">
-          <Grid item>
-            <Avatar
-              src={
-                'https://cdn.discordapp.com/avatars/' +
-                props.user.discord_id +
-                '/' +
-                props.user.discord_avatar +
-                '.png'
-              }
-              style={{
-                margin: 5,
-              }}
-            />
-          </Grid>
-          <Grid item>
-            <Typography>
-              {props.user.discord_name}#{props.user.discord_discriminator} (
-              {props.user.permissions === 1 ? 'Admin' : 'Player'})
-            </Typography>
-          </Grid>
-          <Grid item>Id: {props.user.discord_id}</Grid>
-        </Grid>
-      </Card>
-    </Container>
+    <TableRow key={props.user.discord_id}>
+      <TableCell component="th" scope="row">
+        <Avatar
+          src={
+            'https://cdn.discordapp.com/avatars/' +
+            props.user.discord_id +
+            '/' +
+            props.user.discord_avatar +
+            '.png'
+          }
+          style={{
+            margin: 5,
+          }}
+        />
+      </TableCell>
+      <TableCell>
+        {props.user.discord_name}#{props.user.discord_discriminator}
+      </TableCell>
+      <TableCell>{props.user.discord_id}</TableCell>
+      <TableCell>{props.user.permissions === 1 ? 'Admin' : 'Player'}</TableCell>
+      <TableCell />
+    </TableRow>
   )
 }
--- client/src/views/UserView.tsx
+++ client/src/views/UserView.tsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { UserRow } from '../components/UserRow'
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableCell,
+  TableRow,
+  Paper,
+} from '@material-ui/core'
 import axios from 'axios'
 
 export interface UserRecord {
@@ -21,10 +30,23 @@ export function UserView(): JSX.Element {
     axios.get('/api/user').then(resp => setUsers(resp.data))
   }, [])
   return (
-    <div>
-      {users.map(user => (
-        <UserRow user={user} key={user.discord_id} />
-      ))}
-    </div>
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Avatar</TableCell>
+            <TableCell>Discord Name</TableCell>
+            <TableCell>Discord ID</TableCell>
+            <TableCell>Role</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map(user => (
+            <UserRow user={user} key={user.discord_id} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
