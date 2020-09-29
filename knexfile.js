3f5d27430a14512bbe582e579161bfc1757f06ae
Luiz Sócrate <luiz.socrate@klarna.com>
2020-09-29 22:58:13 +0200

    Fix db connection

--- knexfile.js
+++ knexfile.js
@@ -3,17 +3,33 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
+const extractDatabaseCnfig = /^postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)$/
+
+const connectionParts = extractDatabaseCnfig.exec(env.databaseUrl)
+if (connectionParts === null) {
+  throw Error('Bad database URL')
+}
+const [, user, password, host, port, database] = connectionParts
+const connection = {
+  host,
+  user,
+  password,
+  database,
+  port: parseInt(port, 10),
+  ssl: process.env.NODE_ENV === 'production',
+}
+
 module.exports = {
   development: {
     client: 'pg',
-    connection: `${process.env.DATABASE_URL}`,
+    connection,
     migrations: {
       tableName: 'knex_migrations',
     },
   },
   production: {
     client: 'pg',
-    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    connection,
     migrations: {
       tableName: 'knex_migrations',
     },
--- src/gateways/storage/private/pg.ts
+++ src/gateways/storage/private/pg.ts
@@ -2,9 +2,25 @@ import knex from 'knex'
 
 import env from '../../../env'
 
+const extractDatabaseCnfig = /^postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)$/
+
 export const pg = knex({
   client: 'pg',
-  connection: env.databaseUrl,
+  connection() {
+    const connectionParts = extractDatabaseCnfig.exec(env.databaseUrl)
+    if (connectionParts === null) {
+      throw Error('Bad database URL')
+    }
+    const [, user, password, host, port, database] = connectionParts
+    return {
+      host,
+      user,
+      password,
+      database,
+      port: parseInt(port, 10),
+      ssl: env.nodeEnv === 'production',
+    }
+  },
   migrations: {
     tableName: 'knex_migrations',
   },
