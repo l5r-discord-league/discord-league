ce2345d10dcdae762fb96b0df2bd6bc998f42962
Luiz Sócrate <luiz@socrate.com.br>
2021-01-25 02:15:28 +0100

    Revert "Fix match finished filters"
    This reverts commit b2cb85f95ef86b20f2c32d9b26d3e8c4afb011ab.

--- client/src/components/TournamentMatchView.tsx
+++ client/src/components/TournamentMatchView.tsx
@@ -24,7 +24,6 @@ const useStyles = makeStyles((theme: Theme) =>
 )
 
 const wasPlayedOrSomePlayerDropped = (participants: ParticipantWithUserData[]) => (match: Match) =>
-  match.winnerId !== null ||
   participants.some(
     (participant) =>
       !participant.dropped &&
