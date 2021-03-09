2955c3b899ec7c3232cea3731c91025b116afcb5
Luiz Sócrate <luiz@socrate.com.br>
2021-03-09 20:10:58 +0100

    Restruct tournament index

--- client/src/views/TournamentView/TournamentIndex.tsx
+++ client/src/views/TournamentView/TournamentIndex.tsx
@@ -1,169 +1,49 @@
 import { Tournament } from '@dl/api'
-import {
-  Container,
-  createStyles,
-  Fab,
-  makeStyles,
-  Paper,
-  Tab,
-  Tabs,
-  Theme,
-} from '@material-ui/core'
-import AddIcon from '@material-ui/icons/Add'
-import { memo, useCallback, useReducer, useState } from 'react'
+import { Container, Paper, Tab, Tabs, Theme, createStyles, makeStyles } from '@material-ui/core'
+import { memo, useState } from 'react'
 
-import { api } from '../../api'
-import { MessageSnackBar } from '../../components/MessageSnackBar'
 import { TournamentList } from '../../components/TournamentList'
-import { useIsAdmin } from '../../hooks/useIsAdmin'
-import { EditTournamentModal } from '../../modals/EditTournamentModal'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      position: 'relative',
-    },
     tabs: {
       marginBottom: theme.spacing(2),
     },
-    fab: {
-      position: 'fixed',
-      bottom: theme.spacing(2),
-      right: theme.spacing(2),
-    },
   })
 )
-
-type Action =
-  | { type: 'CLOSE_SNACKBAR' | 'OPEN_MODAL' | 'CLOSE_MODAL' }
-  | { type: 'SUCCESS' | 'FAILURE'; payload: string }
-interface State {
-  snackBarOpen: boolean
-  requestError: boolean
-  snackBarMessage: string
-  modalOpen: boolean
-}
-const initialState: State = {
-  snackBarOpen: false,
-  requestError: false,
-  snackBarMessage: '',
-  modalOpen: false,
-}
-
-function reducer(state: State, action: Action) {
-  switch (action.type) {
-    case 'CLOSE_SNACKBAR':
-      return { ...state, snackBarOpen: false }
-    case 'OPEN_MODAL':
-      return { ...state, modalOpen: true }
-    case 'CLOSE_MODAL':
-      return { ...state, modalOpen: false }
-    case 'SUCCESS':
-      return {
-        snackBarOpen: true,
-        snackBarMessage: action.payload,
-        requestError: false,
-        modalOpen: false,
-      }
-    case 'FAILURE':
-      return {
-        ...state,
-        snackBarOpen: true,
-        snackBarMessage: action.payload,
-        requestError: true,
-      }
-  }
-}
-
-const useCreateTournament = (dispatch: (action: any) => void, onSuccess: () => void) =>
-  useCallback(
-    (name: string, startDate: Date, description?: string) => {
-      api.Tournament.create({
-        body: {
-          name,
-          startDate,
-          description,
-          type: 'pod6',
-          status: 'upcoming',
-        },
-      })
-        .then(() => {
-          dispatch({ type: 'SUCCESS', payload: 'The tournament was created successfully!' })
-          onSuccess()
-        })
-        .catch((res) =>
-          dispatch({
-            type: 'FAILURE',
-            payload: `The tournament could not be created: ${res.error()}`,
-          })
-        )
-    },
-    [dispatch, onSuccess]
-  )
 
 export const TournamentIndex = memo(
   (props: {
     ongoingTournaments: Tournament[]
     pastTournaments: Tournament[]
     upcomingTournaments: Tournament[]
-
-    onCreateTournamentSuccess: () => void
   }) => {
     const classes = useStyles()
-    const isAdmin = useIsAdmin()
-    const [state, dispatch] = useReducer(reducer, initialState)
-    const createTournament = useCreateTournament(dispatch, props.onCreateTournamentSuccess)
     const [activeTab, setActiveTab] = useState<'current' | 'archive'>('current')
 
     return (
-      <div className={classes.root}>
-        <Container>
-          <Paper className={classes.tabs}>
-            <Tabs value={activeTab} onChange={(_, newTab) => setActiveTab(newTab)}>
-              <Tab label="Current" value="current" />
-              <Tab label="Archive" value="archive" />
-            </Tabs>
-          </Paper>
+      <Container>
+        <Paper className={classes.tabs}>
+          <Tabs value={activeTab} onChange={(_, newTab) => setActiveTab(newTab)}>
+            <Tab label="Current" value="current" />
+            <Tab label="Archive" value="archive" />
+          </Tabs>
+        </Paper>
 
-          {activeTab === 'current' && (
-            <>
-              {props.upcomingTournaments.length > 0 && (
-                <TournamentList label="Upcoming" tournaments={props.upcomingTournaments} />
-              )}
-              {props.ongoingTournaments.length > 0 && (
-                <TournamentList label="Ongoing" tournaments={props.ongoingTournaments} />
-              )}
-            </>
-          )}
-          {activeTab === 'archive' && props.pastTournaments.length > 0 && (
-            <TournamentList label="Finished" tournaments={props.pastTournaments} />
-          )}
-        </Container>
-        <EditTournamentModal
-          modalOpen={state.modalOpen}
-          onClose={() => dispatch({ type: 'CLOSE_MODAL' })}
-          onSubmit={createTournament}
-          title="Create new Tournament"
-        />
-        <MessageSnackBar
-          open={state.snackBarOpen}
-          onClose={() => dispatch({ type: 'CLOSE_SNACKBAR' })}
-          error={state.requestError}
-          message={state.snackBarMessage}
-        />
-        {isAdmin && (
-          <Fab
-            color="primary"
-            aria-label="edit"
-            variant="extended"
-            className={classes.fab}
-            onClick={() => dispatch({ type: 'OPEN_MODAL' })}
-          >
-            <AddIcon />
-            New Tournament
-          </Fab>
+        {activeTab === 'current' && (
+          <>
+            {props.upcomingTournaments.length > 0 && (
+              <TournamentList label="Upcoming" tournaments={props.upcomingTournaments} />
+            )}
+            {props.ongoingTournaments.length > 0 && (
+              <TournamentList label="Current" tournaments={props.ongoingTournaments} />
+            )}
+          </>
         )}
-      </div>
+        {activeTab === 'archive' && props.pastTournaments.length > 0 && (
+          <TournamentList label="Archive" tournaments={props.pastTournaments} />
+        )}
+      </Container>
     )
   }
 )
--- client/src/views/TournamentView/TournamentView.tsx
+++ client/src/views/TournamentView/TournamentView.tsx
@@ -1,11 +1,103 @@
+import { Fab, Theme, createStyles, makeStyles } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+import { Dispatch, useCallback, useReducer } from 'react'
+
+import { api } from '../../api'
+import { useIsAdmin } from '../../hooks/useIsAdmin'
 import { useTournaments } from '../../hooks/useTournaments'
-import { RequestError } from '../../components/RequestError'
 import { Loading } from '../../components/Loading'
 import { EmptyState } from '../../components/EmptyState'
+import { MessageSnackBar } from '../../components/MessageSnackBar'
+import { RequestError } from '../../components/RequestError'
+import { EditTournamentModal } from '../../modals/EditTournamentModal'
 import { TournamentIndex } from './TournamentIndex'
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    fab: {
+      position: 'fixed',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+  })
+)
+
+interface State {
+  snackBarOpen: boolean
+  requestError: boolean
+  snackBarMessage: string
+  modalOpen: boolean
+}
+function initState() {
+  return {
+    snackBarOpen: false,
+    requestError: false,
+    snackBarMessage: '',
+    modalOpen: false,
+  }
+}
+
+type Action =
+  | { type: 'CLOSE_SNACKBAR' | 'OPEN_MODAL' | 'CLOSE_MODAL' }
+  | { type: 'SUCCESS' | 'FAILURE'; payload: string }
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'CLOSE_SNACKBAR':
+      return { ...state, snackBarOpen: false }
+    case 'OPEN_MODAL':
+      return { ...state, modalOpen: true }
+    case 'CLOSE_MODAL':
+      return { ...state, modalOpen: false }
+    case 'SUCCESS':
+      return {
+        snackBarOpen: true,
+        snackBarMessage: action.payload,
+        requestError: false,
+        modalOpen: false,
+      }
+    case 'FAILURE':
+      return {
+        ...state,
+        snackBarOpen: true,
+        snackBarMessage: action.payload,
+        requestError: true,
+      }
+  }
+}
+
+const useCreateTournament = (dispatch: Dispatch<Action>, onSuccess: () => void) =>
+  useCallback(
+    (name: string, startDate: Date, description?: string) => {
+      api.Tournament.create({
+        body: {
+          name,
+          startDate,
+          description,
+          type: 'pod6',
+          status: 'upcoming',
+        },
+      })
+        .then(() => {
+          dispatch({ type: 'SUCCESS', payload: 'The tournament was created successfully!' })
+          onSuccess()
+        })
+        .catch(() =>
+          dispatch({
+            type: 'FAILURE',
+            payload: 'The tournament could not be created',
+          })
+        )
+    },
+    [dispatch, onSuccess]
+  )
+
 export function TournamentView() {
+  const classes = useStyles()
+  const [state, dispatch] = useReducer(reducer, undefined, initState)
+  const isAdmin = useIsAdmin()
   const [tournaments, refetchTournaments] = useTournaments()
+  const createTournament = useCreateTournament(dispatch, refetchTournaments)
 
   if (typeof tournaments.error === 'string') {
     return <RequestError requestError={tournaments.error} />
@@ -18,11 +110,36 @@ export function TournamentView() {
   }
 
   return (
-    <TournamentIndex
-      ongoingTournaments={tournaments.data.ongoing}
-      pastTournaments={tournaments.data.past}
-      upcomingTournaments={tournaments.data.upcoming}
-      onCreateTournamentSuccess={refetchTournaments}
-    />
+    <>
+      <TournamentIndex
+        ongoingTournaments={tournaments.data.ongoing}
+        pastTournaments={tournaments.data.past}
+        upcomingTournaments={tournaments.data.upcoming}
+      />
+      <EditTournamentModal
+        modalOpen={state.modalOpen}
+        onClose={() => dispatch({ type: 'CLOSE_MODAL' })}
+        onSubmit={createTournament}
+        title="Create new Tournament"
+      />
+      <MessageSnackBar
+        open={state.snackBarOpen}
+        onClose={() => dispatch({ type: 'CLOSE_SNACKBAR' })}
+        error={state.requestError}
+        message={state.snackBarMessage}
+      />
+      {isAdmin && (
+        <Fab
+          color="primary"
+          aria-label="edit"
+          variant="extended"
+          className={classes.fab}
+          onClick={() => dispatch({ type: 'OPEN_MODAL' })}
+        >
+          <AddIcon />
+          New Tournament
+        </Fab>
+      )}
+    </>
   )
 }
